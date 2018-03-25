37abdae7f26109bfb1f90228b94ab008d3a101da
lucwastiaux <luc.wastiaux@airpost.net>
2018-03-25 18:22:32 +0800

    added configuration at the top

--- code/python/Anki_Quake3.py
+++ code/python/Anki_Quake3.py
@@ -1,16 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Anki Add-on: Progress Bar
-
-Shows progress in the Reviewer in terms of passed cards per session.
-
-Copyright:  (c) Unknown author (nest0r/Ja-Dark?) 2017
-            (c) SebastienGllmt 2017 <https://github.com/SebastienGllmt/>
-            (c) Glutanimate 2017 <https://glutanimate.com/>
-License: GNU AGPLv3 or later <https://www.gnu.org/licenses/agpl.html>
+Anki Add-on: Anki Quake 3
 """
 
-# Do not modify the following lines
 from __future__ import unicode_literals
 
 from anki.hooks import addHook, wrap
@@ -24,9 +16,6 @@ import socket
 from threading import Thread
 import struct
 
-
-import atexit
-
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -37,22 +26,38 @@ global_server_active = True
 
 __version__ = '0.1'
 
+### configuration section
+### =====================
+
+# IP/port where Quake3 is running. the port is the default,
+# you should see it appear in the Quake3 console at startup
+QUAKE3_IP = '127.0.0.1'
+QUAKE3_PORT = 27960
+
+# IP/port where Anki should listen to messages from Quake3
+# if you change those, make sure to change the corresponding host/port 
+# in q3config.cfg:
+# seta cl_ankiHostPort "127.0.0.1:27975"
+ANKI_IP = 'localhost'
+ANKI_PORT = 27975
+
+
+
+### ======================
+
 def getPacketData():
 	return 'FFFFFFFF'.decode('hex') + b'anki_review'
 
 def cardReview():
 	global global_review_count
-	
-	# addressing information of target
-	IPADDR = '127.0.0.1'
-	PORTNUM = 27960
+
 	# enter the data content of the UDP packet as hex
 	PACKETDATA = getPacketData()
 	# initialize a socket, think of it as a cable
 	# SOCK_DGRAM specifies that this is UDP
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
 	# connect the socket, think of it as connecting the cable to the address location
-	s.connect((IPADDR, PORTNUM))
+	s.connect((QUAKE3_IP, QUAKE3_PORT))
 	# send the command
 	s.send(PACKETDATA)
 	# close the socket
@@ -62,15 +67,6 @@ def cardReview():
 	tooltip_text = "Anki Quake3: %d cards left" % actual_review_count
 	tooltip(tooltip_text, period=3000)	
 
-def shutdown_server():
-	global global_socket
-	global global_server_active
-	
-	global_server_active = False
-	
-	global_socket.shutdown()
-	global_socket.close()
-	
 	
 def server_loop():
 	global global_review_count
@@ -82,12 +78,9 @@ def server_loop():
 	try:
 		# Create a TCP/IP socket
 		global_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		#global_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		# global_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
 		# Bind the socket to the port
-		server_address = ('localhost', 27975)
-		#print >>sys.stderr, 'starting up on %s port %s' % server_address
+		server_address = (ANKI_IP, ANKI_PORT)
 		global_socket.bind(server_address)	
 	except socket.error as e:
 		print >> sys.stderr, 'failed to bind to socket:', e
@@ -95,12 +88,9 @@ def server_loop():
 	
 	while global_server_active:
 		data, address = global_socket.recvfrom(1024)
-		
-
 		global_review_count = struct.unpack("i", data)[0]
 		
-		#print >> sys.stderr, "review_count: %d" % global_review_count
-		
+	
 def start_up():
 	thread = Thread(target = server_loop)
 	thread.daemon = True
@@ -109,6 +99,4 @@ def start_up():
 addHook("showAnswer", cardReview)	
 addHook("profileLoaded", start_up)
 		
-# atexit.register(shutdown_server)
-
 
