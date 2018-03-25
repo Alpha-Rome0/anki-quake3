fbd3139cdc62fedb4aae9f861cd91f6544aecb81
lucwastiaux <luc.wastiaux@airpost.net>
2018-03-25 22:36:53 +0800

    cleanups

--- code/python/Anki_Quake3.py
+++ code/python/Anki_Quake3.py
@@ -14,7 +14,7 @@ License: GNU AGPLv3 or later <https://www.gnu.org/licenses/agpl.html>
 
 from __future__ import unicode_literals
 
-from anki.hooks import addHook, wrap
+from anki.hooks import addHook, wrap, runHook
 from anki import version as anki_version
 
 from aqt.qt import *
@@ -83,7 +83,6 @@ orientationHV = Qt.Horizontal # Show bar horizontally (side to side). Use with t
 # orientationHV = Qt.Vertical # Show bar vertically (up and down). Use with right/left dockArea.
 
 pbStyle = ""
-invertTF = False # If set to True, inverts and goes from right to left or top to bottom.
 
 dockArea = Qt.TopDockWidgetArea # Shows bar at the top. Use with horizontal orientation.
 
@@ -109,10 +108,6 @@ def cardReview():
 	# close the socket
 	s.close()
 
-	#actual_review_count = global_review_count - 1
-	#tooltip_text = "Anki Quake3: %d cards left" % actual_review_count
-	#tooltip(tooltip_text, period=3000)	
-
 	
 def server_loop():
 	global g_review_count_target
@@ -134,6 +129,7 @@ def server_loop():
 		unpacked = struct.unpack(b"ii", data)
 		g_review_count_target = unpacked[0]
 		g_review_count_done = unpacked[1]
+		#update_progressbar()
 		
 	
 def start_up():
@@ -164,11 +160,9 @@ def keyHandler(self, evt, _old):
 		
 ## Set up variables for progress bar
 
-nm = 0
 failed = 0
 progressBar = None
 mx = 0
-limitedBarLength = 0
 
 pbdStyle = QStyleFactory.create("%s" % (pbStyle)) # Don't touch.
 
@@ -223,13 +217,12 @@ def _dock(pb):
     return dock
 
 
-def pb():
+def create_progressbar():	
     """Initialize and set parameters for progress bar, adding it to the dock."""
-    mx = max(1, getMX())
     progressBar = QProgressBar()
-    progressBar.setRange(0, mx)
-    progressBar.setTextVisible(showPercent or showNumber)
-    progressBar.setInvertedAppearance(invertTF)
+    progressBar.setTextVisible(True)
+    progressBar.setValue(0)
+    progressBar.setFormat("Quake III Anki - Waiting for connection")	
     progressBar.setOrientation(orientationHV)
     if pbdStyle == None:
         progressBar.setStyleSheet(
@@ -253,75 +246,13 @@ def pb():
         progressBar.setStyle(pbdStyle)
         progressBar.setPalette(palette)
     _dock(progressBar)
-    return progressBar, mx
+    return progressBar
+	
+def setup_progressbar():
+	global progressBar
+	progressBar = create_progressbar()
 
-
-def getMX():
-    """Get deck's card counts for progress bar updates."""
-    rev = nu = lrn = 0
-    if includeRev:
-        rev = mw.col.sched.totalRevForCurrentDeck()
-    if includeLrn:
-        try:
-            lrn = mw.col.sched.lrnCount
-        except AttributeError:
-            pass
-    if includeNew or (includeNewAfterRevs and rev == 0):
-        nu = mw.col.sched.totalNewForCurrentDeck()
-    total = rev + nu + lrn
-    return total
-
-def _getLimitedBarLength():
-    """ Get a new bar length based off the number of new / lrn / rev cards you have left for the day """
-    global limitedBarLength
-    active_decks = mw.col.decks.active()
-    if len(active_decks) > 0:
-        rev = lrn = nu = 0
-
-        # get number of cards
-        for tree in [deck for deck in mw.col.sched.deckDueList() if deck[1] in active_decks]:
-            if includeRev:
-                rev += tree[2]
-            if includeLrn:
-                lrn += tree[3]
-            if includeNew or (includeNewAfterRevs and rev == 0):
-                nu += tree[4]
-            
-        if nu + rev < mx:
-            limitedBarLength = nu+lrn+rev
-            return
-    limitedBarLength = -1
-
-def _updatePB2():
-    """Update progress bar; hiding/showing prevents flashing bug."""
-    global mx
-    if progressBar:
-        total = getMX()
-        if total > mx:
-            mx = total
-            progressBar.setRange(0, mx)
-        curr = (mx - total)
-        progressBar.hide()
-
-        barSize = mx
-        if limitToReviewSettings and limitedBarLength != -1:
-            barSize = limitedBarLength
-
-            # I this can happen if the number of "rev" cards increases during the study session
-            if curr > barSize:
-                barSize = curr
-            progressBar.setRange(0, barSize)
-            
-        if showNumber:
-            if showPercent:
-                percent = 100 if barSize==0 else int(100*curr/barSize)
-                progressBar.setFormat("%d / %d (%d%%)" % (curr, barSize, percent))
-            else:
-                progressBar.setFormat("%d / %d" % (curr, barSize))
-        progressBar.setValue(curr)
-        progressBar.show()
-
-def _updatePB():
+def update_progressbar():
     """Update progress bar; hiding/showing prevents flashing bug."""
     global mx
     if progressBar:
@@ -329,43 +260,21 @@ def _updatePB():
 		if g_review_count_target == -1:
 			# didn't get target review count from quake3
 			progressBar.setValue(0)
-			progressBar.setFormat("Quake III not connected")			
+			progressBar.setFormat("Quake III not connected")
 		else:
 			progressBar.setRange(0, g_review_count_target)
 			progressBar.setValue(g_review_count_done)
-			progressBar.setFormat("Quake III %d / %d" % (g_review_count_done, g_review_count_target))
+			if g_review_count_done < g_review_count_target:
+				progressBar.setFormat("Quake III %d / %d" % (g_review_count_done, g_review_count_target))
+			else:
+				progressBar.setFormat("Quake III all reviews done")
 		
-		
-def _renderBar(state, oldState):
-    global mx, progressBar
-    if state == "overview":
-        # Set up progress bar at deck's overview page: initialize or modify.
-        if not progressBar: progressBar, mx = pb()
-        else: rrenderPB()
-        _getLimitedBarLength()
-        if showNumber:
-            _updatePB()
-        progressBar.show()
-
-
-def rrenderPB():
-    """Modify progress bar if it was already initialized."""
-    global mx
-    if getMX() >= 1:
-        if mx > getMX(): _updatePB()
-        else:
-            mx = getMX()
-            progressBar.setRange(0, mx)
-            progressBar.reset()
-    else: progressBar.setValue(mx)
-
-
 
 
 addHook("showAnswer", cardReview)	
 addHook("profileLoaded", start_up)
-addHook("afterStateChange", _renderBar)
-addHook("showQuestion", _updatePB)		
+addHook("profileLoaded", setup_progressbar)
+addHook("showQuestion", update_progressbar)
 
 Reviewer._keyHandler = wrap(Reviewer._keyHandler, keyHandler, "around")
 		
